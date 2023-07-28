56e78c06100ed82a8874faca9bd3433650bbfa5f
RahmanIslamIen <rahmanislamien@gmail.com>
2023-07-28 13:44:56 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <h1>Welcome to my website</h1>
+        <p>This is the content of the website.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
